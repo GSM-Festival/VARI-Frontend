02556ca8cd412882060e5a91bc8cc6fc462487e2
eodn <s20063@gsm.hs.kr>
2021-12-23 11:25:00 +0900

    SignIn-Publishing

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,10 +1,26 @@
 import * as S from "./Style";
 import * as I from "../../Assets";
+import { Link } from "react-router-dom";
 
 const Signin = () => {
   return (
     <S.Positioner>
-      <I.LogoSmall />
+      <S.LogoWrapper>
+        <I.LogoSmall />
+      </S.LogoWrapper>
+      <S.LabelStyle>Email</S.LabelStyle>
+      <S.InputWrapper>
+        <S.InputStyle placeholder="이메일을 입력하세요." />
+      </S.InputWrapper>
+      <S.LabelStyle>Password</S.LabelStyle>
+      <S.InputStyle placeholder="비밀번호를 입력하세요." />
+      <S.ButtonStyle>
+        <Link to="/">로그인</Link>
+      </S.ButtonStyle>
+      <S.SignupContainer>
+        <span>아직 계정이 없으신가요?</span>
+        <Link to="/signup">회원가입</Link>
+      </S.SignupContainer>
     </S.Positioner>
   );
 };
--- src/Components/SignIn/Style.js
+++ src/Components/SignIn/Style.js
@@ -5,17 +5,61 @@ export const Positioner = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  min-height: 100vh;
 `;
 
-export const Logo = styled.div`
+export const LogoWrapper = styled.div`
+  margin-top: 152px;
+  margin-bottom: 143px;
+`;
+
+export const InputWrapper = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column;
+  margin-bottom: 28px;
 `;
 
-export const a = styled.div`
-  width: 192px;
-  height: 248px;
-  background: #000;
+export const LabelStyle = styled.label`
+  width: 100%;
+  margin-left: 85px;
+  font-size: 22px;
+  font-weight: 500;
+`;
+
+export const InputStyle = styled.input`
+  margin-top: 10px;
+  width: 500px;
+  height: 60px;
+  background-color: #f3f3f3;
+  border-radius: 10px;
+  border: none;
+  padding-left: 10px;
+`;
+
+export const ButtonStyle = styled.button`
+  margin-top: 88px;
+  width: 500px;
+  height: 60px;
+  cursor: pointer;
+  background-color: #395bb4;
+  border-radius: 10px;
+  border: none;
+  color: #fff;
+  font-size: 24px;
+  font-weight: 700;
+  a {
+    text-decoration: none;
+    color: #fff;
+  }
+`;
+
+export const SignupContainer = styled.div`
+  margin-top: 200px;
+  span {
+    color: #9f9f9f;
+  }
+  a {
+    margin-left: 7px;
+    text-decoration: none;
+    color: #3a4c98;
+  }
 `;
