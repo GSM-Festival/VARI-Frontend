66421fac4b6953e61c32ae84f44f2d1f4cae538b
eodn <s20063@gsm.hs.kr>
2021-12-26 23:45:14 +0900

    Completion-Header

--- src/Components/Common/Header/Header.js
+++ src/Components/Common/Header/Header.js
@@ -1,9 +1,19 @@
 import * as S from "./Style";
+import * as I from "../../../Assets";
+import HeaderLogo from "../../../Assets/SVG/HeaderLogo.svg";
 
 const Header = () => {
   return (
     <>
-      <S.Positioner></S.Positioner>
+      <S.Positioner>
+        <S.LogoWrapper>
+          <img src={HeaderLogo} />
+        </S.LogoWrapper>
+        <S.Member>
+          정대우<strong>님</strong>
+        </S.Member>
+        <I.Message />
+      </S.Positioner>
     </>
   );
 };
--- src/Components/Common/Header/Style.js
+++ src/Components/Common/Header/Style.js
@@ -3,6 +3,23 @@ import styled from "styled-components";
 export const Positioner = styled.div`
   width: 100%;
   height: 100px;
+  display: flex;
+  align-items: center;
   background: #ffffff;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const LogoWrapper = styled.div`
+  margin-left: 19px;
+`;
+
+export const Member = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  color: #395bb4;
+  strong {
+    margin-left: 3px;
+    color: #395bb4;
+    font-size: 17px;
+  }
 `;
