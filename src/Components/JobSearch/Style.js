a8cde17e525b4f332ac652d5991d22349faf7c03
eodn <s20063@gsm.hs.kr>
2021-12-28 20:38:24 +0900

    Add write button

--- src/Components/JobSearch/Style.js
+++ src/Components/JobSearch/Style.js
@@ -2,32 +2,37 @@ import styled, { css } from "styled-components";
 
 export const Positioner = styled.div`
   height: 100%;
+  width: 100%;
   background-color: #f9f9f9;
   overflow: scroll;
+
   ::-webkit-scrollbar {
     display: none; /* Chrome, Safari, Opera */
   }
+  display: flex;
+  justify-content: center;
 `;
 
 export const Topic = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  width: 440px;
+  margin-top: 80px;
 `;
 
 export const TopicBtn = styled.button`
-  width: 100px;
-  height: 50px;
-  outline: none;
+  height: 42px;
   border: none;
   background: none;
   font-size: 20px;
   font-weight: 600;
   color: #b1b1b1;
+  cursor: pointer;
   ${(props) =>
     props.active &&
     css`
-      border-bottom: 3px solid #395bb4;
+      border-bottom: 6px solid #395bb4;
       color: #395bb4;
     `}
 `;
@@ -41,4 +46,18 @@ export const TopicList = styled.div`
 export const TopicItem = styled.div`
   font-size: 18px;
   border: 1px solid blue;
+`;
+
+export const WriteBtn = styled.button`
+  background-color: #fff;
+  border-radius: 50px;
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  margin-left: 390px;
+  top: 933px;
+  border: none;
+  cursor: pointer;
+  transform: scaleX(-1);
+  box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.12);
 `;
